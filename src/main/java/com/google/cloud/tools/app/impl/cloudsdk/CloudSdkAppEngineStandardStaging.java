076ce62d4e381e75ac4a399f4bb9c7a951cc59ab
Appu Goundan <appu@google.com>
2016-05-24 15:15:27 -0400

    Copy dockerfile into source directory before staging (#82)
    * Copy dockerfile into source directory before staging

--- src/main/java/com/google/cloud/tools/app/impl/cloudsdk/CloudSdkAppEngineStandardStaging.java
+++ src/main/java/com/google/cloud/tools/app/impl/cloudsdk/CloudSdkAppEngineStandardStaging.java
@@ -66,17 +66,16 @@ public class CloudSdkAppEngineStandardStaging implements AppEngineStandardStagin
     arguments.add(config.getSourceDirectory().toPath().toString());
     arguments.add(config.getStagingDirectory().toPath().toString());
 
-    Path dockerfile =
-        config.getDockerfile() == null ? null : config.getDockerfile().toPath();
-    Path dockerfileDestination = config.getStagingDirectory().toPath();
+    Path dockerfile = config.getDockerfile() == null ? null : config.getDockerfile().toPath();
 
     try {
 
-      cloudSdk.runAppCfgCommand(arguments);
-
       if (dockerfile != null && dockerfile.toFile().exists()) {
-        Files.copy(dockerfile, dockerfileDestination, StandardCopyOption.REPLACE_EXISTING);
+        Files.copy(dockerfile, config.getSourceDirectory().toPath(),
+            StandardCopyOption.REPLACE_EXISTING);
       }
+
+      cloudSdk.runAppCfgCommand(arguments);
 
     } catch (IOException | ProcessRunnerException e) {
       throw new AppEngineException(e);
