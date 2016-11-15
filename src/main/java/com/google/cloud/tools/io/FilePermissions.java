cf123119b4b5e2395e9d05d0e14e1f81fc45b4ee
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2016-11-15 16:14:53 -0500

    File Utilities that don't depend on eclipse (#249)
    * add FilePermissions class and tests
    
    * beta annotation
    
    * import order
    
    * import blank line
    
    * path --> segment
    
    * handle non-regular files
    
    * ignore test on windows

---
+++ src/main/java/com/google/cloud/tools/io/FilePermissions.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.io;
+
+import com.google.common.annotations.Beta;
+
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Non-IDE specific utility methods for working with file systems.
+ * We hope to migrate these into Guava. Clients should beware that these methods are
+ * not yet stable and may change without notice. 
+ */
+@Beta
+public class FilePermissions {
+
+  /**
+   * Check whether the current process can create a directory at the specified path.
+   * This is useful for providing immediate feedback to an end user that a path they have 
+   * selected or typed may not be suitable before attempting to create the directory;
+   * e.g. in a tooltip.
+   * 
+   * @param path tentative location for directory
+   * @throws IOException if a directory cannot be created at the specified path
+   */
+  public static void verifyDirectoryCreatable(Path path) throws IOException {
+    
+    for (Path segment = path; segment != null; segment = segment.getParent()) {
+      if (Files.exists(segment)) {
+        if (Files.isDirectory(segment)) {
+          // Can't create a directory if the bottom most currently existing directory in
+          // the path is not writable. 
+          if (!Files.isWritable(segment)) {
+            throw new IOException(segment + " is not writable");
+          }
+        } else { 
+          // Can't create a directory if a non-directory file already exists with that name
+          // somewhere in the path. 
+          throw new FileAlreadyExistsException(segment + " is a file");  
+        }
+        break;
+      }
+    }
+    
+  }
+
+}
---
+++ src/test/java/com/google/cloud/tools/io/FilePermissionsTest.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.io;
+
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+
+public class FilePermissionsTest {
+
+  private Path parent;
+  
+  @Before
+  public void setUp() throws IOException {
+     parent = Files.createTempDirectory("foo");
+  }
+
+  @Test
+  public void testDirectoryCanBeCreated() throws IOException {
+    FilePermissions.verifyDirectoryCreatable(Paths.get(parent.toString(), "bar"));
+  }
+
+  @Test
+  public void testSubDirectoryCanBeCreated() throws IOException {
+    FilePermissions.verifyDirectoryCreatable(Paths.get(parent.toString(), "bar", "baz"));
+  }
+  
+  @Test // Unix only
+  public void testSubDirectoryCannotBeCreatedInDevNull()  {
+    Assume.assumeTrue(!System.getProperty("os.name").startsWith( "Windows"));
+    try {
+      FilePermissions.verifyDirectoryCreatable(Paths.get("/dev/null/foo/bar"));
+      Assert.fail("Can create directory in /dev/null");
+    } catch (IOException ex) {
+      Assert.assertTrue(ex.getMessage(), ex.getMessage().contains("/dev/null"));
+    }
+  }
+  
+  @Test
+  public void testDirectoryCannotBeCreatedDueToPreexistingFile() throws IOException {
+    Path file = Files.createTempFile(parent, "prefix", "suffix");
+    try {
+      FilePermissions.verifyDirectoryCreatable(file);
+      Assert.fail("Can create directory over file");
+    } catch (FileAlreadyExistsException ex) {
+      Assert.assertTrue(ex.getMessage().contains(file.getFileName().toString()));
+    }
+  }
+  
+  @Test
+  public void testSubDirectoryCannotBeCreatedDueToPreexistingFile() throws IOException {
+    Path file = Files.createTempFile(parent, "prefix", "suffix");
+    try {
+      FilePermissions.verifyDirectoryCreatable(Paths.get(file.toString(), "bar", "baz"));
+      Assert.fail("Can create directory over file");
+    } catch (FileAlreadyExistsException ex) {
+      Assert.assertTrue(ex.getMessage().contains(file.getFileName().toString()));
+    }
+  }
+  
+  @Test
+  public void testDirectoryCannotBeCreatedDueToUnwritableParent() throws IOException {
+    Path dir = Files.createDirectory(Paths.get(parent.toString(), "child"));
+    dir.toFile().setWritable(false);
+    try {
+      FilePermissions.verifyDirectoryCreatable(Paths.get(dir.toString(), "bar"));
+      Assert.fail("Can create directory in non-writable parent");
+    } catch (IOException ex) {
+      Assert.assertTrue(ex.getMessage().contains(dir.getFileName().toString()));
+    }
+  }
+  
+  @Test
+  public void testRootNotWritable() throws IOException {
+    try {
+      FilePermissions.verifyDirectoryCreatable(Paths.get("/bar"));
+      Assert.fail("Can create directory in root");
+    } catch (IOException ex) {
+      Assert.assertEquals("/ is not writable", ex.getMessage());
+    }
+  }
+
+}
