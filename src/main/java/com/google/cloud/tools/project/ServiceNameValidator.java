0bef8f3d44a2716e783bbce57bd5ccb6d857ff4d
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2016-11-28 10:16:56 -0500

    validator for App Engine service (a.k.a. module) names (#260)
    * validator for App Engine service (a.k.a. module) names
    
    * isLetterOrDigit
    
    * javadoc

---
+++ src/main/java/com/google/cloud/tools/project/ServiceNameValidator.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.project;
+
+import com.google.common.annotations.Beta;
+
+/**
+ * Each service and each version must have a name. A name can contain numbers, letters, and hyphens.
+ * It cannot be longer than 63 characters and cannot start or end with a hyphen."
+ */
+@Beta
+public class ServiceNameValidator {
+
+  /**
+   * @param name App Engine service (a.k.a. module) name
+   * @return true if and only if the name meets the constraints for servcie names; false otherwise
+   */
+  public static boolean validate(String name) {
+    if (name == null) {
+      return false;
+    } else if (name.isEmpty()) {
+      return false;
+    } else if (name.startsWith("-")) {
+      return false;
+    } else if (name.endsWith("-")) {
+      return false;
+    } else if (name.length() > 63) {
+      return false;
+    } else {
+      for (char c : name.toCharArray()) {
+        if (Character.isLetterOrDigit(c)) {
+          continue;
+        } else if (c == '-') {
+          continue;
+        } else {
+          return false;
+        }
+      }
+    }
+    
+    return true;
+  }
+
+}
---
+++ src/test/java/com/google/cloud/tools/project/ServiceNameValidatorTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.project;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ServiceNameValidatorTest {
+  
+  @Test
+  public void testDomain() {
+    Assert.assertFalse(ServiceNameValidator.validate("google.com:mystore"));
+  }
+
+  @Test
+  public void testPartition() {
+    Assert.assertFalse(ServiceNameValidator.validate("s~google.com:mystore"));
+  }
+
+  @Test
+  public void testOneWord() {
+    Assert.assertTrue(ServiceNameValidator.validate("word"));
+  }
+
+  @Test
+  public void testUpperCase() {
+    Assert.assertTrue(ServiceNameValidator.validate("WORD"));
+  }
+  
+  @Test
+  public void testLongWord() {
+    boolean validate = ServiceNameValidator.validate(
+        "012345678901234567890123456789012345678901234567890123456789"
+        + "012345678901234567890123456789012345678901234567890");
+    Assert.assertFalse(validate);
+  }
+  
+  @Test
+  public void testContainsSpace() {
+    Assert.assertFalse(ServiceNameValidator.validate("com google eclipse"));
+  }
+
+  @Test
+  public void testEmptyString() {
+    Assert.assertFalse(ServiceNameValidator.validate(""));
+  }
+  
+  @Test
+  public void testNull() {
+    Assert.assertFalse(ServiceNameValidator.validate(null));
+  }
+
+  @Test
+  public void testBeginsWithHyphen() {
+    Assert.assertFalse(ServiceNameValidator.validate("-foo"));
+  }
+  
+  @Test
+  public void testEndsWithHyphen() {
+    Assert.assertFalse(ServiceNameValidator.validate("-bar"));
+  }
+  
+  @Test
+  public void testContainsHyphen() {
+    Assert.assertTrue(ServiceNameValidator.validate("foo-bar"));
+  }
+}
