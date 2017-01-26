b5772efb2a547587d4a185ee892c793300804904
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2017-01-26 13:20:42 -0500

    Add AppEngineDescriptor class (#307)
    * import order recs are out of date with respect to current google style
    
    * copy over AppEngineDescriptor from CT4E
    
    * final
    
    * return null instead of throwing an exception
    
    * add missing @Test annotations
    
    * remove untrhown exception
    
    * remove unused import

---
+++ src/main/java/com/google/cloud/tools/appengine/AppEngineDescriptor.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.appengine;
+
+import java.io.IOException;
+import java.io.InputStream;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+/**
+ * Utilities to obtain information from appengine-web.xml.
+ */
+public class AppEngineDescriptor {
+
+  private static final String APP_ENGINE_NAMESPACE = "http://appengine.google.com/ns/1.0";
+  private final Document document;
+  
+  // private to force use of parse method
+  private AppEngineDescriptor(Document document) {
+    this.document = document;
+  }
+
+  /**
+   * Parses an appengine-web.xml file.
+   * 
+   * @param in the contents of appengine-web.xml
+   * @return a fully parsed object that can be queried 
+   * @throws IOException if parsing fails for any reason including malformed XML
+   */
+  public static AppEngineDescriptor parse(InputStream in) throws IOException {
+    try {
+      DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+      documentBuilderFactory.setNamespaceAware(true);
+      return new AppEngineDescriptor(documentBuilderFactory.newDocumentBuilder().parse(in));
+    } catch (SAXException | ParserConfigurationException exception) {
+      throw new IOException("Cannot parse appengine-web.xml", exception);
+    }
+  }
+
+  /**
+   * @return project ID from the &lt;application&gt; element of the appengine-web.xml or null
+   *         if it is missing
+   */
+  public String getProjectId()  {
+    return getTopLevelValue(document, "appengine-web-app", "application");
+  }
+
+  /**
+   * @return project version from the &lt;version&gt; element of the appengine-web.xml or
+   *         null if it is missing
+   */
+  public String getProjectVersion() {
+    return getTopLevelValue(document, "appengine-web-app", "version");
+  }
+
+  /**
+   * @return service ID from the &lt;service&gt; element of the appengine-web.xml, or
+   *         null if it is missing. Will also look at module ID.
+   */
+  public String getServiceId() {
+    String serviceId = getTopLevelValue(document, "appengine-web-app", "service");
+    if (serviceId != null) {
+      return serviceId;
+    }
+    return getTopLevelValue(document, "appengine-web-app", "module");
+  }
+
+  private static String getTopLevelValue(Document doc, String parentTagName, String childTagName) {
+    try {
+      NodeList parentElements = doc.getElementsByTagNameNS(APP_ENGINE_NAMESPACE, parentTagName);
+      if (parentElements.getLength() > 0) {
+        Node parent = parentElements.item(0);
+        if (parent.hasChildNodes()) {
+          for (int i = 0; i < parent.getChildNodes().getLength(); ++i) {
+            Node child = parent.getChildNodes().item(i);
+            if (child.getNodeName().equals(childTagName)) {
+              return child.getTextContent();
+            }
+          }
+        }
+      }
+      return null;
+    } catch (DOMException ex) {
+      // this shouldn't happen barring a very funky DOM implementation
+      return null;
+    }
+  }
+}
---
+++ src/test/java/com/google/cloud/tools/appengine/AppEngineDescriptorTest.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.tools.appengine;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.junit.Test;
+
+public class AppEngineDescriptorTest {
+
+  private static final String TEST_VERSION = "fooVersion";
+  private static final String TEST_ID = "fooId";
+
+  private static final String ROOT_END_TAG = "</appengine-web-app>";
+  private static final String ROOT_START_TAG =
+      "<appengine-web-app xmlns='http://appengine.google.com/ns/1.0'>";
+  private static final String ROOT_START_TAG_WITH_INVALID_NS =
+      "<appengine-web-app xmlns='http://foo.bar.com/ns/42'>";
+  private static final String PROJECT_ID = "<application>" + TEST_ID + "</application>";
+  private static final String VERSION = "<version>" + TEST_VERSION + "</version>";
+  private static final String COMMENT = "<!-- this is a test comment -->";
+  private static final String COMMENT_AFTER_VERSION = "<version>" + TEST_VERSION + COMMENT + "</version>";
+  private static final String COMMENT_BEFORE_VERSION = "<version>" + COMMENT + TEST_VERSION + "</version>";
+  private static final String SERVICE = "<service>" + TEST_ID + "</service>";
+  private static final String MODULE = "<module>" + TEST_ID + "</module>";
+  
+  private static final String XML_WITHOUT_PROJECT_ID = ROOT_START_TAG + ROOT_END_TAG;
+  private static final String XML_WITHOUT_VERSION = ROOT_START_TAG + PROJECT_ID + ROOT_END_TAG;
+  private static final String XML_WITH_VERSION_AND_PROJECT_ID =
+      ROOT_START_TAG + PROJECT_ID + VERSION + ROOT_END_TAG;
+  private static final String XML_WITH_COMMENT_BEFORE_VERSION =
+      ROOT_START_TAG + PROJECT_ID + COMMENT_BEFORE_VERSION + ROOT_END_TAG;
+  private static final String XML_WITH_COMMENT_AFTER_VERSION =
+      ROOT_START_TAG + PROJECT_ID + COMMENT_AFTER_VERSION + ROOT_END_TAG;
+  private static final String XML_WITH_VERSION_AND_PROJECT_ID_WRONG_NS =
+      ROOT_START_TAG_WITH_INVALID_NS + PROJECT_ID + VERSION + ROOT_END_TAG;
+
+  @Test
+  public void testParse_noProjectId() throws IOException {
+    AppEngineDescriptor descriptor = parse(XML_WITHOUT_PROJECT_ID);
+
+    assertNull(descriptor.getProjectId());
+  }
+
+  @Test
+  public void testParse_noVersion() throws IOException {
+    AppEngineDescriptor descriptor = parse(XML_WITHOUT_VERSION);
+
+    assertEquals(TEST_ID, descriptor.getProjectId());
+    assertNull(descriptor.getProjectVersion());
+  }
+
+  @Test
+  public void testParse_properXml() throws IOException {
+    AppEngineDescriptor descriptor = parse(XML_WITH_VERSION_AND_PROJECT_ID);
+
+    assertEquals(TEST_ID, descriptor.getProjectId());
+    assertEquals(TEST_VERSION, descriptor.getProjectVersion());
+  }
+
+  @Test
+  public void testParse_xmlWithCommentBeforeValue() throws IOException {
+    AppEngineDescriptor descriptor = parse(XML_WITH_COMMENT_BEFORE_VERSION);
+
+    assertEquals(TEST_ID, descriptor.getProjectId());
+    assertEquals(TEST_VERSION, descriptor.getProjectVersion());
+  }
+
+  @Test
+  public void testParse_xmlWithCommentAfterValue() throws IOException {
+    AppEngineDescriptor descriptor = parse(XML_WITH_COMMENT_AFTER_VERSION);
+
+    assertEquals(TEST_ID, descriptor.getProjectId());
+    assertEquals(TEST_VERSION, descriptor.getProjectVersion());
+  }
+  
+  @Test
+  public void testParse_xmlWithInvalidNamespace() throws IOException {
+    AppEngineDescriptor descriptor = parse(XML_WITH_VERSION_AND_PROJECT_ID_WRONG_NS);
+
+    assertNull(descriptor.getProjectId());
+    assertNull(descriptor.getProjectVersion());
+  }
+
+  @Test
+  public void testService_noContent() throws IOException {
+    AppEngineDescriptor descriptor = parse(ROOT_START_TAG + ROOT_END_TAG);
+
+    assertNull(descriptor.getServiceId());
+  }
+
+  @Test
+  public void testService_service() throws IOException {
+    AppEngineDescriptor descriptor = parse(ROOT_START_TAG + SERVICE + ROOT_END_TAG);
+
+    assertEquals(TEST_ID, descriptor.getServiceId());
+  }
+
+  @Test
+  public void testService_module() throws IOException {
+    AppEngineDescriptor descriptor = parse(ROOT_START_TAG + MODULE + ROOT_END_TAG);
+
+    assertEquals(TEST_ID, descriptor.getServiceId());
+  }
+
+  private static AppEngineDescriptor parse(String xmlString) throws IOException {
+    return AppEngineDescriptor
+        .parse(new ByteArrayInputStream(xmlString.getBytes(StandardCharsets.UTF_8)));
+  }
+
+}
