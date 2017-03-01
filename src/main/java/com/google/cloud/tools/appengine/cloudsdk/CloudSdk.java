c64d8a16aa6da0bd21c7fff4e460617ee1e75632
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2017-03-01 05:06:06 -0500

    upgrade minimum SDK version (#334)

--- src/main/java/com/google/cloud/tools/appengine/cloudsdk/CloudSdk.java
+++ src/main/java/com/google/cloud/tools/appengine/cloudsdk/CloudSdk.java
@@ -62,7 +62,7 @@ import javax.annotation.Nullable;
  */
 public class CloudSdk {
 
-  public static final CloudSdkVersion MINIMUM_VERSION = new CloudSdkVersion("133.0.0");
+  public static final CloudSdkVersion MINIMUM_VERSION = new CloudSdkVersion("145.0.0");
 
   private static final Logger logger = Logger.getLogger(CloudSdk.class.getName());
   private static final Joiner WHITESPACE_JOINER = Joiner.on(" ");
