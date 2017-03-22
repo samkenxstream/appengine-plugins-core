08384ab8cadf5b7d4bc4fa025b4167d3141e84ad
Andras Kerekes <akerekes@users.noreply.github.com>
2017-03-22 18:01:36 -0400

    More fixes to make the kokoro build successful (#350)
    * update gcloud, install appengine component, fix comment
    
    * use 'sudo /opt/google-cloud-sdk/bin/gcloud'

--- kokoro/continuous.sh
+++ kokoro/continuous.sh
@@ -6,4 +6,6 @@ set -e
 set -x
 
 cd github/appengine-plugins-core
+sudo /opt/google-cloud-sdk/bin/gcloud components update
+sudo /opt/google-cloud-sdk/bin/gcloud components install app-engine-java
 mvn clean install -B -U
