81931359dc1ed691bf9e83fdbb94fd7699e4eebd
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2017-09-08 12:53:00 -0400

    expand JSON test coverage; add datastore and cloud storage (#443)
    * expand JSON test coverage
    
    * add test for equals
    
    * hamcrest
    
    * redundant assert removed
    
    * fix loop
    
    * fix loop
    
    * regex versions
    
    * don't make repo a corrdinate
    
    * type to packaging
    
    * verify group ID artifact ID
    
    * checkstyle fixes
    
    * revert metadata for now
    
    * code review
    
    * add datastore api
    
    * toArray
    
    * GCS
    
    * make transport an array
    
    * make transport an array
    
    * launchStage
    
    * pluralize transports
    
    * pluralize transports
    
    * add packages

--- src/test/java/com/google/cloud/tools/libraries/LibrariesTest.java
+++ src/test/java/com/google/cloud/tools/libraries/LibrariesTest.java
@@ -1,4 +1,22 @@
-package com.google.cloud.tools.libraries;
+/*
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+ package com.google.cloud.tools.libraries;
+
+import static org.hamcrest.collection.IsArrayContaining.hasItemInArray;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -7,24 +25,35 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Iterator;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.junit.Assert;
-import org.junit.Test;
-import org.xml.sax.SAXException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.json.JsonReaderFactory;
-import javax.json.JsonValue;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.xml.sax.SAXException;
 
 public class LibrariesTest {
+
+  private JsonObject[] apis;
+
+  @Before
+  public void parseJson() throws FileNotFoundException {    
+    JsonReaderFactory factory = Json.createReaderFactory(null);
+    InputStream in =
+        new FileInputStream("src/main/java/com/google/cloud/tools/libraries/libraries.json");
+    JsonReader reader = factory.createReader(in); 
+    apis = reader.readArray().toArray(new JsonObject[0]); 
+  }
 
   @Test
   public void testWellFormed() throws ParserConfigurationException, SAXException, IOException {
@@ -36,27 +65,59 @@ public class LibrariesTest {
   }
   
   @Test
-  public void testJson() throws FileNotFoundException, URISyntaxException {
-    JsonReaderFactory factory = Json.createReaderFactory(null);
-    InputStream in =
-        new FileInputStream("src/main/java/com/google/cloud/tools/libraries/libraries.json");
-    JsonReader reader = factory.createReader(in); 
-    Iterator<JsonValue> apis = reader.readArray().iterator();
-    Assert.assertTrue(apis.hasNext());
-    for (JsonObject api = (JsonObject) apis.next(); apis.hasNext(); api = (JsonObject) apis.next()) {
-    
-      new URI(api.getString("documentation"));
-      if (api.getString("icon") != null) {
-        new URI(api.getString("icon"));
+  public void testJson() throws URISyntaxException {
+    Assert.assertTrue(apis.length > 0);
+    for (int i = 0; i < apis.length; i++) { 
+      assertApi(apis[i]);
+    }
+  }
+
+  private static final String[] statuses = {"early access", "alpha", "beta", "GA", "deprecated"};
+
+  private static void assertApi(JsonObject api) throws URISyntaxException {
+    Assert.assertFalse(api.getString("name").isEmpty());
+    Assert.assertFalse(api.getString("description").isEmpty());
+    String transports = api.getJsonArray("transports").getString(0);
+    Assert.assertTrue(transports + " is not a recognized transport",
+        "http".equals(transports) || "grpc".equals(transports));
+    new URI(api.getString("documentation"));
+    if (api.getString("icon") != null) {
+      new URI(api.getString("icon"));
+    }
+    JsonArray clients = api.getJsonArray("clients");
+    Assert.assertFalse(clients.isEmpty());
+    for (int i = 0; i < clients.size(); i++) {
+      JsonObject client = (JsonObject) clients.get(i);
+      String launchStage = client.getString("launchStage");
+      Assert.assertThat(statuses, hasItemInArray(launchStage));
+      new URI(client.getString("apireference"));
+      new URI(client.getString("site"));
+      Assert.assertTrue(client.getString("languageLevel").matches("1\\.\\d+\\.\\d+"));
+      Assert.assertFalse(client.getString("name").isEmpty());
+      Assert.assertNotNull(client.getJsonObject("mavenCoordinates"));
+      if (client.getString("source") != null) {
+        new URI(client.getString("source"));
       }
-      JsonArray clients = api.getJsonArray("clients");
-      Assert.assertFalse(clients.isEmpty());
-      for (int i = 0; i < clients.size(); i++) {
-        JsonObject client = (JsonObject) clients.get(i);
-        String status = client.getString("status");
-        Assert.assertTrue(status, 
-            "beta".equals(status) || "alpha".equals(status) || "GA".equals(status));
+    }
+  }
+  
+  @Test
+  public void testDuplicates() throws URISyntaxException {
+    Map<String, String> apiCoordinates = new HashMap<>();
+    for (int i = 0; i < apis.length; i++) { 
+      JsonObject api = apis[i];
+      String name = api.getString("name");
+      if (apiCoordinates.containsKey(name)) {
+        Assert.fail(name + " is defined twice");
       }
+      JsonObject coordinates =
+          ((JsonObject) api.getJsonArray("clients").get(0)).getJsonObject("mavenCoordinates");
+      String mavenCoordinates = 
+          coordinates.getString("groupId") + ":" + coordinates.getString("artifactId");
+      if (apiCoordinates.containsValue(mavenCoordinates)) {
+        Assert.fail(mavenCoordinates + " is defined twice");
+      }
+      apiCoordinates.put(name, mavenCoordinates);
     }
   }
 
