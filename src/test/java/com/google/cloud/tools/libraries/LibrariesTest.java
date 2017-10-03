2cf1ad4b08e31d7ff7aca8c5a1284eb900cdba16
Chanseok Oh <chanseok@google.com>
2017-10-03 12:45:53 -0400

    Delete proof-of-concept libraries.xml (#464)

--- src/test/java/com/google/cloud/tools/libraries/LibrariesTest.java
+++ src/test/java/com/google/cloud/tools/libraries/LibrariesTest.java
@@ -18,13 +18,11 @@ package com.google.cloud.tools.libraries;
 
 import static org.hamcrest.collection.IsArrayContaining.hasItemInArray;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -33,13 +31,9 @@ import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.json.JsonReaderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.xml.sax.SAXException;
 
 public class LibrariesTest {
 
@@ -52,15 +46,6 @@ public class LibrariesTest {
         new FileInputStream("src/main/java/com/google/cloud/tools/libraries/libraries.json");
     JsonReader reader = factory.createReader(in);
     apis = reader.readArray().toArray(new JsonObject[0]);
-  }
-
-  @Test
-  public void testWellFormed() throws ParserConfigurationException, SAXException, IOException {
-    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-    factory.setNamespaceAware(true);
-    DocumentBuilder parser = factory.newDocumentBuilder();
-    File in = new File("src/main/java/com/google/cloud/tools/libraries/libraries.xml");
-    parser.parse(in);
   }
 
   @Test
@@ -113,7 +98,7 @@ public class LibrariesTest {
   }
 
   @Test
-  public void testDuplicates() throws URISyntaxException {
+  public void testDuplicates() {
     Map<String, String> apiCoordinates = new HashMap<>();
     for (JsonObject api : apis) {
       String name = api.getString("name");
