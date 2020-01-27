b40d832ca146528a27a43b5a0368a9c3248b3a02
Etan Shaul <eshaul@google.com>
2020-01-27 17:22:08 -0500

    try CLOUDSDK_PYTHON again

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -2,7 +2,8 @@
 
 REM Java 9 does not work with our builds right now, force java 8
 set JAVA_HOME=c:\program files\java\jdk1.8.0_152
-set PATH=%JAVA_HOME%\bin;%PATH%
+set CLOUDSDK_PYTHON=c:\python27\python.exe
+set PATH=%CLOUDSDK_PYTHON%;%JAVA_HOME%\bin;%PATH%
 
 cd github/appengine-plugins-core
 
