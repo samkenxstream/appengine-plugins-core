f542be9cc5d38e1b42b7617c8543b150feabf88b
Etan Shaul <eshaul@google.com>
2020-01-27 17:27:11 -0500

    revert python path change again. not the right fix

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -2,8 +2,7 @@
 
 REM Java 9 does not work with our builds right now, force java 8
 set JAVA_HOME=c:\program files\java\jdk1.8.0_152
-set CLOUDSDK_PYTHON=c:\python27\python.exe
-set PATH=%CLOUDSDK_PYTHON%;%JAVA_HOME%\bin;%PATH%
+set PATH=%JAVA_HOME%\bin;%PATH%
 
 cd github/appengine-plugins-core
 
